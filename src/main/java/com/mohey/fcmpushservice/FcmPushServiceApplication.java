5e2b1934ba78b57cf0f5f2f19eda7c0c47e4656d
genboyjy <genboyjy@gmail.com>
2023-07-27 15:11:41 +0900

    initial commit

---
+++ src/main/java/com/mohey/fcmpushservice/FcmPushServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mohey.fcmpushservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmPushServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmPushServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohey/fcmpushservice/FcmPushServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohey.fcmpushservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmPushServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
